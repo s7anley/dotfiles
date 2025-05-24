724c32ffedde75f5abed8551068f24580abb5494
Jany Koščo <jan.kosco@productboard.com>
2025-05-24 20:41:27 +0200

    Initial fish config

---
+++ link/fish/aliases.fish
@@ -0,0 +1,8 @@
+alias g git
+abbr gi git
+abbr gti git
+abbr v vim
+abbr bwre brew
+abbr brwe brew
+abbr d docker
+alias z zellij
---
+++ link/fish/config.fish
@@ -0,0 +1,4 @@
+source ~/.config/fish/aliases.fish
+
+# don't show any greetings
+set fish_greeting ""
